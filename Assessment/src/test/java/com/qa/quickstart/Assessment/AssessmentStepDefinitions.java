b3e70ee3f603696823c88f563e927a77ef516a98
Hussein Ahmed Tejan <tejanahmedhu@gmail.com>
2018-06-22 13:17:24 +0100

    added few steps

--- Assessment/src/test/java/com/qa/quickstart/Assessment/AssessmentStepDefinitions.java
+++ Assessment/src/test/java/com/qa/quickstart/Assessment/AssessmentStepDefinitions.java
@@ -1,5 +1,33 @@
 package com.qa.quickstart.Assessment;
 
+import org.openqa.selenium.WebDriver;
+
+import com.relevantcodes.extentreports.ExtentReports;
+
 public class AssessmentStepDefinitions {
+	
+	WebDriver driver;
+	
+	static ExtentReports extent;
+	
+	@Before
+	public void setUp() {
+		
+	}
+	
+	
+	@Given("^the Add Employee Tab")
+	public void
+	
+	@When("")
+	
+	@And("")
+	
+	@And("")
+	
+	@Then("")
+	
+	
+	
 
 }
